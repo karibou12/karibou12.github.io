38c8425183dfe196f75c5c8b5ffff883ad6250bf
karibou12 <57873752+karibou12@users.noreply.github.com>
2019-12-28 22:22:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
+//barre de nav avce li lien etc
+//******************************
 
-function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-    
+//Creation balise header
+var headerElt = document.createElement("header");
+headerElt.id= "header";
+document.getElementById("main").insertAdjacentHTML("afterbegin", '<header id="header"></header>');
+
+//Creation balise nav
+var navElt = document.createElement("nav");
+navElt.id= "nav";
+document.getElementById("header").appendChild(navElt);
+
+//Creation balise ul
+var ulElt = document.createElement("ul");
+ulElt.id= "ul";
+document.getElementById("nav").appendChild(ulElt);
+
+
+// creation liste dans ul
+var Liste = ["Accueil","Galerie"];
+var maNavListe = ["index.html","galerie.html"];
+
+for (var i = 0; i < Liste.length; i++) {
+    var lienElt = document.createElement("li"); 
+    lienElt.id = Liste[i];
+    var lienA = document.createElement("a")
+    lienA.textContent = Liste[i];
+    lienA.href = maNavListe[i];
+    // Ajoute un lien
+    document.getElementById("ul").appendChild(lienElt);
+    // Ajoute une balise <a>
+    document.getElementById(Liste[i]).appendChild(lienA);
 }
+
+
+//Galerie 
+/**********/
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+} 
+
+
+
+
+
+
 
 
    
